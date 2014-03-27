2c616221464b67af482ff03521d958622fd53154
Lance West <lancewest@hotmail.com>
2014-03-26 23:50:11 -0500

    Fixed moveing and initialization

--- Functional Prototype/server.js
+++ Functional Prototype/server.js
@@ -1,6 +1,4 @@
-// This node.js program implements a simple chat room service.
-
-// The node.js HTTP server.
+//Server For Team 3's Chinese Checkers Project
 var express = require("express");
 var app = express();
 var port = 11657;
@@ -15,76 +13,41 @@ var io = require('socket.io').listen(app.listen(port));
 // server. At that time, the function (the 2nd argument) will be called with an
 // object representing the client.
 io.sockets.on(
-  'connection',
-  function(client) {
-    // Send a welcome message first.
-    client.emit('welcome', 'Welcome to my Chinese Checkers!');
+	'connection',
+	function(client) {
+		// Send a welcome message first.
+		client.emit('welcome', 'Welcome to my Chinese Checkers!');
 
-    // Listen to an event called 'login'. The client should emit this event when
-    // it wants to log in to the chat room.
-    client.on(
-      'login',
-      function(message) {
-        // This function extracts the user name from the login message, stores
-        // it to the client object, sends a login_ok message to the client, and
-        // sends notifications to other clients.
-        if (message && message.user_name) {
-		  players.push(client);
-          client.set('user_name', message.user_name);
-          client.emit('login_ok');
-          // client.broadcast.emits() will send to all clients except the
-          // current client. See socket.io FAQ for more examples.
-          client.broadcast.emit('notification',
-                                message.user_name + ' entered the room.');
-								
-		  if(players.length%2 == 0) {
-			players[players.length-1].emit('start_game');
-			players[players.length-2].emit('start_game');
-		  }
-          return
-        }
-        // When something is wrong, send a login_failed message to the client.
-        client.emit('login_failed');
-      });
+		// Listen to an event called 'login'. The client should emit this event when
+		// it wants to log in to the chat room.
+		client.on(
+			'login',
+			function(message) {
+				// This function extracts the user name from the login message, stores
+				// it to the client object, sends a login_ok message to the client
+				if (message && message.user_name) {
+					players.push(client);
+					client.set('user_name', message.user_name);
+					client.emit('login_ok');
+				
+					//When there are 2 Clients Waiting, start a game
+					if(players.length%2 == 0) {
+						players[players.length-1].emit('start_game');
+						players[players.length-2].emit('start_game');
+					}
+					return;
+				}
+				// When something is wrong, send a login_failed message to the client.
+				client.emit('login_failed');
+			});
 
-    // Listen to an event called 'chat'. The client should emit this event when
-    // it sends a chat message.
-    client.on(
-      'chat',
-      function(message) {
-        // This function tries to get the user name from the client object, and
-        // use that to form a chat message that will be sent to all clients.
-        if (message && message.msg) {
-          client.get(
-            'user_name', 
-            function(err, name) {
-              if (!err) {
-                // io.sockets.emit() will send the message to all clients,
-                // including the current client. See socket.io FAQ for more
-                // examples.
-                io.sockets.emit('chat', { user_name: name, msg: message.msg });
-              }
-            });
-        }
-      });
 	  
-	  client.on(
-      'move',
-      function(startX, startY, endX, endY) {
-          client.broadcast.emit('move', startX, startY, endX, endY);
-      });
-
-    // Print a message when somebody left.
-    client.on(
-      'disconnect',
-      function() {
-        client.get(
-          'user_name',
-          function(err, name) {
-            if (name) {
-              io.sockets.emit('notification', name + ' left the room.');
-            }
-          });
-      });
-  });
+	// Listen to an event called 'move'. The client should emit this event when
+	// it has moved a marble and then the server will pass it on to the other clients
+	client.on(
+    'move',
+    function(startX, startY, endX, endY) {
+        client.broadcast.emit('move', startX, startY, endX, endY);
+    });
+});
 
