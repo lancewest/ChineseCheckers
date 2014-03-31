b264deb3cce837383da29eb7fd6cfa3f2ae8d3b8
Lance West <lancewest@hotmail.com>
2014-03-31 15:35:17 -0500

    Added More Than 2 Players, and matchmaking, and multigames at once

--- Functional Prototype/server.js
+++ Functional Prototype/server.js
@@ -1,12 +1,67 @@
 //Server For Team 3's Chinese Checkers Project
 var express = require("express");
 var app = express();
-var port = 21657;
-var players = new Array();
+var port = 22657;
+
+//This Matrix holds all the players waiting to get into a game. The players are sorted into the Matrix by the number of players 
+// that they want to be in a game with. If they are looking for Any, then they are in the first row, if they are looking for 2 
+// then they are in the second row and so on...
+var waitingPlayers = new Array(7);
+for(var i = 0; i<7; i++)
+	waitingPlayers[i] = new Array();
 
 app.use("/", express.static(__dirname));
 
 var io = require('socket.io').listen(app.listen(port));
+
+
+//Helper Function to match players with simular settings
+function findPlayersForGame() {
+	var players = [ ]
+	
+	for(var numPlayers = 6; numPlayers > 0; numPlayers--) {
+		if(numPlayers == 5)
+			numPlayers--;
+	
+		if(numPlayers == 1) {
+			if(waitingPlayers[1].length >= 3)
+				for(var i = 0; i<3; i++)
+					players = waitingPlayers[1].pop();
+		}
+		else if(waitingPlayers[numPlayers].length + waitingPlayers[1].length >= numPlayers) { //If we have enough to make a game of numPlayers size
+			players = waitingPlayers[numPlayers].concat(waitingPlayers[1]);
+			waitingPlayers[1] = [ ];
+			waitingPlayers[numPlayers] = [ ];
+		}
+		
+		//Here is where we will eventually narrow the player list down based on gamemode and timer options, 
+		// then check to see if we still have at least the desired number
+	}
+
+	return players;
+}
+
+//Helper Function to find board position of the person who is moving given numPlayers and relativePosition to client
+function getBoardPosition(relativePosition, numPlayers) {
+	if(numPlayers == 6)
+		return relativePosition;
+	
+	if(numPlayers == 4) {
+		if(relativePosition == 1)
+			return 2;
+		else if(relativePosition == 2)
+			return 3
+		else if(relativePosition == 3)
+			return 5;
+	}
+	
+	if(numPlayers == 3)
+		return relativePosition*2
+		
+	if(numPlayers == 2)
+		return 3;
+}
+
 
 // Tells socket.io to listen to an event called 'connection'.
 // This is a built-in event that is triggered when a client connects to the
@@ -22,19 +77,38 @@ io.sockets.on(
 		// it wants to log in to the chat room.
 		client.on(
 			'login',
-			function(name) {
+			function(name, numPlayers) {
 				// This function extracts the user name from the login message, stores
 				// it to the client object, sends a login_ok message to the client
 				if (name != null) {
-					players.push(client);
+					waitingPlayers[numPlayers].push(client);
 					client.user_name = name;
+					client.numPlayers = numPlayers;
+					client.otherPlayers = [ ];
+					
 					client.emit('login_ok');
-				
-					//When there are 2 Clients Waiting, start a game
-					if(players.length%2 == 0) {
-						players[players.length-1].emit('start_game', "your_turn");
-						players[players.length-2].emit('start_game', "opponents_turn");
+					
+					var playersInGame = findPlayersForGame();
+					if(playersInGame != null && playersInGame.length > 1) {
+					
+						for(var i = 0; i<playersInGame.length; i++) {
+							if(i == playersInGame.length-1)
+								playersInGame[i].next = playersInGame[0];
+							else
+								playersInGame[i].next = playersInGame[i+1];
+							
+							if(i == 0)
+								playersInGame[i].emit('start_game', "Your Turn!", playersInGame.length, i);
+							else
+								playersInGame[i].emit('start_game', playersInGame[0].user_name + "'s Turn.", playersInGame.length, i);
+								
+							for(var j = 0; j<playersInGame.length; j++) {
+								if(i != j)
+									playersInGame[i].otherPlayers.push(playersInGame[j]);
+							}	
+						}
 					}
+					
 					return;
 				}
 				// When something is wrong, send a login_failed message to the client.
@@ -47,7 +121,24 @@ io.sockets.on(
 	client.on(
     'move',
     function(startX, startY, endX, endY) {
-        client.broadcast.emit('move', startX, startY, endX, endY);
+		client.emit('you_moved', client.next.user_name + "'s Turn.");
+	
+		for(var i = 0; i<client.otherPlayers.length; i++) {
+		
+			/*var iterator = client.otherPlayers[i].next;
+			var relativePositionOfMover = 1;
+			while(iterator != client) {
+				relativePositionOfMover++;
+				iterator = iterator.next;
+			}
+			
+			var boardPosition = getBoardPosition(relativePositionOfMover, client.otherPlayers.length+1);*/
+		
+			if(client.otherPlayers[i] == client.next)
+				client.otherPlayers[i].emit('move', startX, startY, endX, endY, "Your Turn!");
+			else
+				client.otherPlayers[i].emit('move', startX, startY, endX, endY, client.next.user_name + "'s Turn.");
+		}
     });
 	
 	// Listen to an event called 'win'. The client should emit this event when
